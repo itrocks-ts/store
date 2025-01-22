e167e25ce3a66db5adde89251191fb1be1208f45
Baptiste Pillot <baptiste@pillot.fr>
2025-01-22 12:03:56 +0100

    v0.0.1

---
+++ README.md
@@ -0,0 +1,7 @@
+[![npm version](https://img.shields.io/npm/v/@itrocks/store?logo=npm)](https://www.npmjs.org/package/@itrocks/store)
+[![npm downloads](https://img.shields.io/npm/dm/@itrocks/store)](https://www.npmjs.org/package/@itrocks/store)
+[![GitHub](https://img.shields.io/github/last-commit/itrocks-ts/store?color=2dba4e&label=commit&logo=github)](https://github.com/itrocks-ts/store)
+[![issues](https://img.shields.io/github/issues/itrocks-ts/store)](https://github.com/itrocks-ts/store/issues)
+[![discord](https://img.shields.io/discord/1314141024020467782?color=7289da&label=discord&logo=discord&logoColor=white)](https://25.re/ditr)
+
+# store
---
+++ src/store.ts
@@ -0,0 +1,37 @@
+import { baseType, ObjectOrType, Type }  from '@itrocks/class-type'
+import { decorateCallback, decoratorOf } from '@itrocks/decorator/class'
+
+const STORE = Symbol('store')
+
+type Dependencies = {
+	setTransformers?: (target: Type) => void
+	toStoreName:      (className: string) => string
+}
+
+const depends: Dependencies = {
+	toStoreName: targetName => targetName
+}
+
+export function setDependencies(dependencies: Partial<Dependencies>)
+{
+	Object.assign(depends, dependencies)
+}
+
+export default Store
+export function Store(name: string | false = '')
+{
+	return decorateCallback(STORE, function(target) {
+		if ((name !== false) && depends.setTransformers) {
+			depends.setTransformers(target)
+		}
+		if (name !== '') {
+			return name
+		}
+		return depends.toStoreName(baseType(target).name)
+	})
+}
+
+export function storeOf(target: ObjectOrType)
+{
+	return decoratorOf<string | false>(target, STORE, false)
+}
